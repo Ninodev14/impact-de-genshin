df863530ec88bb630be06ab9a167f2a6fed948e3
Ninodev14 <115539009+Ninodev14@users.noreply.github.com>
2024-01-10 11:10:47 +0100

    changement de font

--- js/night_day.js
+++ js/night_day.js
@@ -22,46 +22,64 @@ document.addEventListener("DOMContentLoaded", function () {
   const dayNav = document.getElementById("day-nav");
   const sond = document.getElementById("sond");
   const sondNav = document.getElementById("sond-nav");
-  const h1 = document.getElementById("h1");
+  const h1Titre = document.getElementById("h1-titre");
+  const h2Titre = document.getElementById("h2-titre");
   const h2 = document.getElementById("h2");
+  const p = document.querySelectorAll("p");
+  const gradient = document.getElementById("gradient-overlay");
   const separationHeader = document.getElementById("separation-svg-header");
   const body = document.body;
   const dayHomePage = document.getElementById("dayHomePage");
   const nightHomePage = document.getElementById("nightHomePage");
   const backgroundMusic = document.getElementById("backgroundMusic");
+  const footer = document.getElementById("footer");
  
   function handleDayClick() {
     if ( sky == 0){
       body.classList.remove("night-mode");
+      body.style.backgroundColor = "#212222"
+      footer.style.backgroundColor = "#000000"
+      gradient.style.background = "linear-gradient(0deg, #212222 1.48%, rgba(33, 34, 34, 0.00) 22.74%)";    
       dayHomePage.style.display = "block";
       nightHomePage.style.display = "none";
       day.src = "image/black-night.svg";
-      dayNav.src = "image/black-night.svg";
-      h1.style.color = "#212222";
-      h1.style.textShadow = "0px 0px 8px rgba(f, f, f, 0.35)";
-      h2.style.color = "#212222";
+      dayNav.src = "image/white-night.svg";
+      h1Titre.style.color = "#212222";
+      h1Titre.style.textShadow = "0px 0px 8px rgba(f, f, f, 0.35)";
+      h2Titre.style.color = "#212222";
+      h2.style.color = "#fff";
+      p.forEach(p => {
+        p.style.color = "#fff";
+      });
       separationHeader.src = "image/header-limitation-black.svg";
       sky = 1;
       if(etat == 1) {
       sond.src = "image/black-mute.svg";
-      sondNav.src = "image/black-mute.svg";
+      sondNav.src = "image/white-mute.svg";
       color = 0;
     } else {
       sond.src = "image/black-on.svg";
-      sondNav.src = "image/black-on.svg";
+      sondNav.src = "image/white-on.svg";
       color = 0;
     }
   }else {
-
+    gradient.style.background = "linear-gradient(0deg, #FFF 1.48%, rgba(255, 255, 255, 0.00) 22.74%)";
+    footer.style.backgroundColor = "#212222"
+    body.style.backgroundColor = "#fff"
     body.classList.add("night-mode");
     dayHomePage.style.display = "none";
     nightHomePage.style.display = "block";
     day.src = "image/white-day.svg";
     dayNav.src = "image/white-day.svg";
-    h1.style.color = "#fff";
-    h1.style.textShadow = "0px 0px 8px rgba(0, 0, 0, 0.35)";
-    h2.style.color = "#fff";
+    h1Titre.style.color = "#fff";
+    h1Titre.style.textShadow = "0px 0px 8px rgba(0, 0, 0, 0.35)";
+    h2Titre.style.color = "#fff";
+    h2.style.color = "#212222";
+    p.forEach(p => {
+      p.style.color = "#212222";
+    });
     separationHeader.src = "image/header-limitation-white.svg";
+
     sky = 0;
     if(etat == 1) {
     sond.src = "image/white-mute.svg";
@@ -91,7 +109,7 @@ function toggleMusic() {
       sondNav.src = "image/white-mute.svg";
     } else {
       sond.src = "image/black-mute.svg";
-      sondNav.src = "image/black-mute.svg";
+      sondNav.src = "image/white-mute.svg";
     }
     etat = 1;
   } else {
@@ -100,7 +118,7 @@ function toggleMusic() {
       sondNav.src = "image/white-on.svg";
     } else {
       sond.src = "image/black-on.svg";
-      sondNav.src = "image/black-on.svg";
+      sondNav.src = "image/white-on.svg";
     }
     etat = 0;
   }
